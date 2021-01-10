8301d5fac012ac0f4fef921f91553902f0ed5d91
HARSHITSINGHANIA <75519213+HARSHITSINGHANIA@users.noreply.github.com>
2021-01-10 11:50:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,7 +33,8 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-    log6 = new Log(300,180,150,PI/2)
+    log6 = new Log(300,180,150,PI/2);
+    log7 = new Log(380,120,150,PI/2); 
 
     bird = new Bird(100,100);
 
@@ -60,6 +61,7 @@ function draw(){
     log4.display();
     log5.display();
     log6.display();
+    log7.display();
 
     bird.display();
     platform.display();
